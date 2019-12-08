8560a03a2942737c46598d753b8541de34182cfc
sufyankamal <37336667+sufyankamal@users.noreply.github.com>
2019-12-09 02:14:26 +0800

    Add files via upload

--- lib/tab_screen4.dart
+++ lib/tab_screen4.dart
@@ -7,19 +7,23 @@ import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:flutter/services.dart';
 import 'loginscreen.dart';
+import 'payment.dart';
 import 'registrationscreen.dart';
 import 'splashscreen.dart';
 import 'user.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:toast/toast.dart';
 import 'package:image_picker/image_picker.dart';
-import 'mainscreen.dart';
+import 'package:intl/intl.dart';
+import 'package:random_string/random_string.dart';
+
 
 String urlgetuser = "http://expectojr.com/mygardener/php/get_user.php";
 String urluploadImage = "http://expectojr.com/mygardener/php/upload_imageprofile.php";
 String urlupdate = "http://expectojr.com/mygardener/php/update_profile.php";
 File _image;
 int number = 0;
+String _value;
 
 class TabScreen4 extends StatefulWidget {
   //final String email;
@@ -53,7 +57,7 @@ class _TabScreen4State extends State<TabScreen4> {
         home: Scaffold(
           resizeToAvoidBottomPadding: false,
           body: ListView.builder(
-              //Step 6: Count the data
+            //Step 6: Count the data
               itemCount: 5,
               itemBuilder: (context, index) {
                 if (index == 0) {
@@ -63,7 +67,7 @@ class _TabScreen4State extends State<TabScreen4> {
                         Stack(children: <Widget>[
                           Image.asset(
                             "assets/images/background.png",
-                            fit: BoxFit.fitHeight,
+                            fit: BoxFit.fitWidth,
                           ),
                           Column(
                             children: <Widget>[
@@ -84,7 +88,7 @@ class _TabScreen4State extends State<TabScreen4> {
                                     height: 150.0,
                                     decoration: new BoxDecoration(
                                         shape: BoxShape.circle,
-                                        border: Border.all(color: Colors.black),
+                                        border: Border.all(color: Colors.white),
                                         image: new DecorationImage(
                                             fit: BoxFit.cover,
                                             image: new NetworkImage(
@@ -116,7 +120,7 @@ class _TabScreen4State extends State<TabScreen4> {
                                       Icon(
                                         Icons.phone_android,
                                       ),
-                                      Text(widget.user.phone??
+                                      Text(widget.user.phone ??
                                           'not registered'),
                                     ],
                                   ),
@@ -137,7 +141,7 @@ class _TabScreen4State extends State<TabScreen4> {
                                     initialRating: double.parse(
                                         widget.user.rating.toString() ?? 0.0),
                                     itemPadding:
-                                        EdgeInsets.symmetric(horizontal: 2.0),
+                                    EdgeInsets.symmetric(horizontal: 2.0),
                                     itemBuilder: (context, _) => Icon(
                                       Icons.star,
                                       color: Colors.amber,
@@ -154,8 +158,8 @@ class _TabScreen4State extends State<TabScreen4> {
                                         Icons.rounded_corner,
                                       ),
                                       Text("Job Radius " +
-                                              widget.user.radius +
-                                              "KM" ??
+                                          widget.user.radius +
+                                          "KM" ??
                                           'Job Radius 0 KM'),
                                     ],
                                   ),
@@ -174,8 +178,8 @@ class _TabScreen4State extends State<TabScreen4> {
                                       ),
                                       Flexible(
                                         child: Text("You have " +
-                                                widget.user.credit +
-                                                " Credit" ??
+                                            widget.user.credit +
+                                            " Credit" ??
                                             "You have 0 Credit"),
                                       ),
                                     ],
@@ -244,6 +248,7 @@ class _TabScreen4State extends State<TabScreen4> {
                           child: Text("CHANGE RADIUS"),
                         ),
                         MaterialButton(
+                          onPressed: _loadPayment,
                           child: Text("BUY CREDIT"),
                         ),
                         MaterialButton(
@@ -255,7 +260,7 @@ class _TabScreen4State extends State<TabScreen4> {
                           child: Text("LOG IN"),
                         ),
                         MaterialButton(
-                          onPressed: _logout,
+                          onPressed: _gotologout,
                           child: Text("LOG OUT"),
                         )
                       ],
@@ -286,7 +291,7 @@ class _TabScreen4State extends State<TabScreen4> {
               onPressed: () async {
                 Navigator.of(context).pop();
                 _image =
-                    await ImagePicker.pickImage(source: ImageSource.camera);
+                await ImagePicker.pickImage(source: ImageSource.camera);
 
                 String base64Image = base64Encode(_image.readAsBytesSync());
                 http.post(urluploadImage, body: {
@@ -340,7 +345,7 @@ class _TabScreen4State extends State<TabScreen4> {
 
       setState(() {
         _currentAddress =
-            "${place.name},${place.locality}, ${place.postalCode}, ${place.country}";
+        "${place.name},${place.locality}, ${place.postalCode}, ${place.country}";
         //load data from database into list array 'data'
       });
     } catch (e) {
@@ -349,6 +354,11 @@ class _TabScreen4State extends State<TabScreen4> {
   }
 
   void _changeRadius() {
+    if (widget.user.name == "not register") {
+      Toast.show("Not allowed", context,
+          duration: Toast.LENGTH_LONG, gravity: Toast.BOTTOM);
+      return;
+    }
     TextEditingController radiusController = TextEditingController();
 
     showDialog(
@@ -358,7 +368,7 @@ class _TabScreen4State extends State<TabScreen4> {
         return AlertDialog(
           title: new Text("Change new Radius (km)?"),
           content: new TextField(
-              keyboardType: TextInputType.phone,
+              keyboardType: TextInputType.number,
               controller: radiusController,
               decoration: InputDecoration(
                 labelText: 'new radius',
@@ -432,7 +442,7 @@ class _TabScreen4State extends State<TabScreen4> {
       builder: (BuildContext context) {
         // return object of type Dialog
         return AlertDialog(
-          title: new Text("Change " + widget.user.name),
+          title: new Text("Change name for " + widget.user.name + "?"),
           content: new TextField(
               controller: nameController,
               decoration: InputDecoration(
@@ -460,16 +470,17 @@ class _TabScreen4State extends State<TabScreen4> {
                     print('in success');
                     setState(() {
                       widget.user.name = dres[1];
-                      if (dres[0] == "success") {
-                        print("in setstate");
-                        widget.user.name = dres[1];
-                      }
                     });
+                    Toast.show("Success", context,
+                        duration: Toast.LENGTH_LONG, gravity: Toast.BOTTOM);
+                    Navigator.of(context).pop();
+                    return;
                   } else {}
                 }).catchError((err) {
                   print(err);
                 });
-                Navigator.of(context).pop();
+                Toast.show("Failed", context,
+                    duration: Toast.LENGTH_LONG, gravity: Toast.BOTTOM);
               },
             ),
             new FlatButton(
@@ -530,15 +541,14 @@ class _TabScreen4State extends State<TabScreen4> {
                       if (dres[0] == "success") {
                         Toast.show("Success", context,
                             duration: Toast.LENGTH_LONG, gravity: Toast.BOTTOM);
-                            savepref(passController.text);
-                            Navigator.of(context).pop();
+                        savepref(passController.text);
+                        Navigator.of(context).pop();
                       }
                     });
                   } else {}
                 }).catchError((err) {
                   print(err);
                 });
-                
               },
             ),
             new FlatButton(
@@ -583,7 +593,7 @@ class _TabScreen4State extends State<TabScreen4> {
                 if (phoneController.text.length < 5) {
                   Toast.show("Please enter correct phone number", context,
                       duration: Toast.LENGTH_LONG, gravity: Toast.BOTTOM);
-                      return;
+                  return;
                 }
                 http.post(urlupdate, body: {
                   "email": widget.user.email,
@@ -600,7 +610,6 @@ class _TabScreen4State extends State<TabScreen4> {
                       return;
                     });
                   }
-                  
                 }).catchError((err) {
                   print(err);
                 });
@@ -657,7 +666,6 @@ class _TabScreen4State extends State<TabScreen4> {
   }
 
   void _gotologinPage() {
-    TextEditingController phoneController = TextEditingController();
     // flutter defined function
     print(widget.user.name);
     showDialog(
@@ -673,9 +681,6 @@ class _TabScreen4State extends State<TabScreen4> {
               child: new Text("Yes"),
               onPressed: () {
                 Navigator.of(context).pop();
-                print(
-                  phoneController.text,
-                );
                 Navigator.push(
                     context,
                     MaterialPageRoute(
@@ -694,9 +699,127 @@ class _TabScreen4State extends State<TabScreen4> {
     );
   }
 
+  void _gotologout() async {
+    // flutter defined function
+    print(widget.user.name);
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        // return object of type Dialog
+        return AlertDialog(
+          title: new Text("Go to login page?" + widget.user.name),
+          content: new Text("Are your sure?"),
+          actions: <Widget>[
+            // usually buttons at the bottom of the dialog
+            new FlatButton(
+              child: new Text("Yes"),
+              onPressed: () async {
+                Navigator.of(context).pop();
+                SharedPreferences prefs = await SharedPreferences.getInstance();
+                await prefs.setString('email', '');
+                await prefs.setString('pass', '');
+                print("LOGOUT");
+                Navigator.pop(context,
+                    MaterialPageRoute(builder: (context) => SplashScreen()));
+              },
+            ),
+            new FlatButton(
+              child: new Text("No"),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   void savepref(String pass) async {
     print('Inside savepref');
     SharedPreferences prefs = await SharedPreferences.getInstance();
-        await prefs.setString('pass', pass);
+    await prefs.setString('pass', pass);
+  }
+
+  void _loadPayment() async {
+    // flutter defined function
+    if (widget.user.name == "not register") {
+      Toast.show("Not allowed please register", context,
+          duration: Toast.LENGTH_LONG, gravity: Toast.BOTTOM);
+      return;
+    }
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        // return object of type Dialog
+        return AlertDialog(
+          title: new Text("Buy Credit?"),
+          content: Container(
+            height: 100,
+            child: DropdownExample(),
+          ),
+          actions: <Widget>[
+            // usually buttons at the bottom of the dialog
+            new FlatButton(
+              child: new Text("Yes"),
+              onPressed: () async {
+                Navigator.of(context).pop();
+                var now = new DateTime.now();
+                var formatter = new DateFormat('ddMMyyyyhhmmss-');
+                String formatted = formatter.format(now)+randomAlphaNumeric(10);
+                print(formatted);
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => PaymentScreen(user:widget.user,orderid:formatted, val:_value)));
+              },
+            ),
+            new FlatButton(
+              child: new Text("No"),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
+
+class DropdownExample extends StatefulWidget {
+  @override
+  _DropdownExampleState createState() {
+    return _DropdownExampleState();
+  }
+}
+
+class _DropdownExampleState extends State<DropdownExample> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: DropdownButton<String>(
+        items: [
+          DropdownMenuItem<String>(
+            child: Text('50 Credit (RM10)'),
+            value: '10',
+          ),
+          DropdownMenuItem<String>(
+            child: Text('100 Credit (RM20)'),
+            value: '20',
+          ),
+          DropdownMenuItem<String>(
+            child: Text('150 Credit (RM30)'),
+            value: '30',
+          ),
+        ],
+        onChanged: (String value) {
+          setState(() {
+            _value = value;
+          });
+        },
+        hint: Text('Select Credit'),
+        value: _value,
+      ),
+    );
   }
 }
